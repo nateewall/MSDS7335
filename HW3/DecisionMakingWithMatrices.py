dae3cb62c038dec140c30272c606fbf22d2bb2ce
Wall <bsnxw01@railinc.com>
2019-02-25 17:11:44 -0500

    add heatmap

--- HW3/DecisionMakingWithMatrices.py
+++ HW3/DecisionMakingWithMatrices.py
@@ -8,9 +8,16 @@
 import random
 import names
 import numpy as np
+import os
 from scipy.stats import rankdata
+import matplotlib.pyplot as plt
 
 random.seed(3)
+#create the master project file
+projectName = 'RestaurantSelection'
+
+if not os.path.exists(projectName):
+    os.makedirs(projectName)
 
 print("You asked your 10 work friends to answer a survey. They gave you back the following dictionary object.")
 print('')
@@ -132,34 +139,36 @@ print(M_restaurants)
 print(M_restaurants.dtype)
 print('')
 
-print("----------------------------------------------------------------------------------------------------")
-print("The most imporant idea in this project is the idea of a linear combination.")
-print('')
-print("Informally describe what a linear combination is and how it will relate to our restaurant matrix.")
-print('')
-print('Linear combination is the idea that given a matrix of data and corresponding weights of each feature of that matrix we can determine optimal decision/ranking')
-print('')
-print('Alternatively, given a matrix or vector and the scores/rankings we can determine the weights that would produce that output')
-print('')
-print('So for our examples given the characteristics of several restaurants and the weights each person places on those characteristics we can determine an optimal restaurant')
-print('')
-print('Similarly if we are given those restaurant charactertics and the all the peoples rankings of the rest. we can determine everyones preferences')
-print('')
+# print("----------------------------------------------------------------------------------------------------")
+# print("The most imporant idea in this project is the idea of a linear combination.")
+# print('')
+# print("Informally describe what a linear combination is and how it will relate to our restaurant matrix.")
+# print('')
+# print('Linear combination is the idea that given a matrix of data and corresponding weights of each feature of that matrix we can determine optimal decision/ranking')
+# print('')
+# print('Alternatively, given a matrix or vector and the scores/rankings we can determine the weights that would produce that output')
+# print('')
+# print('So for our examples given the characteristics of several restaurants and the weights each person places on those characteristics we can determine an optimal restaurant')
+# print('')
+# print('Similarly if we are given those restaurant charactertics and the all the peoples rankings of the rest. we can determine everyones preferences')
+# print('')
+#
+# print("----------------------------------------------------------------------------------------------------")
+# print("Choose a person and compute (using a linear combination) the top restaurant for them.  What does each entry in the resulting vector represent.")
 
-print("----------------------------------------------------------------------------------------------------")
-print("Choose a person and compute (using a linear combination) the top restaurant for them.  What does each entry in the resulting vector represent.")
-# x.view(np.float64).reshape(x.shape + (-1,))
+
 R = M_restaurants.view(int).reshape(len(M_restaurants),-1)
 P = M_people.view(int).reshape(len(M_people),-1)
-person0score = np.dot(R, P[0])
-restScore = sorted(zip(rNames, map(lambda x: round(x, 4), list(person0score))), reverse=True, key = lambda t: t[1])
 
-print('')
-print("The top restaurant for %s and score is:" % list(people.keys())[0])
-print(restScore[0])
-print('')
-print("Each score in the resulting vector represents their compatibility with that restaurant based on the similarity between their preferences and the restaurants characteristics")
-print('')
+# person0score = np.dot(R, P[0])
+# restScore = sorted(zip(rNames, map(lambda x: round(x, 4), list(person0score))), reverse=True, key = lambda t: t[1])
+#
+# print('')
+# print("The top restaurant for %s and score is:" % list(people.keys())[0])
+# print(restScore[0])
+# print('')
+# print("Each score in the resulting vector represents their compatibility with that restaurant based on the similarity between their preferences and the restaurants characteristics")
+# print('')
 
 print("----------------------------------------------------------------------------------------------------")
 print("Next compute a new matrix (M_usr_x_rest  i.e. an user by restaurant) from all people.  What does the a_ij matrix represent?")
@@ -194,40 +203,101 @@ M_usr_x_rest_rank = tmp.reshape(len(M_people),-1)
 print(M_usr_x_rest_rank)
 print('')
 
-rankScore = np.sum(M_usr_x_rest_rank, axis = 0)
-print(rankScore)
-rankScoreRest = sorted(zip(rNames, map(lambda x: round(x, 4), list(rankScore))), reverse=True, key = lambda t: t[1])
-print('The final sorted restaurant by ranks scores are shown below, higher values mean more compatible for the group.')
-for r in rankScoreRest:
-    print(r)
-print('')
-
-print("----------------------------------------------------------------------------------------------------")
-print("Why is there a difference between the two?  What problem arrives?  What does represent in the real world?")
-print("")
-print("The most compatible restaurant for the group is different between the raw sum & the sum on the order statistics")
-print("")
-print("This is driven by the fact that certain people have really high raw scores for the restaurant 'Recess' however, that rank value helps reduce the impact of those elevated scores")
-print("")
-print("In the real world this could be related to the people that may have really strong preferences can often times sway the entire group to their preferences.")
+# rankScore = np.sum(M_usr_x_rest_rank, axis = 0)
+# print(rankScore)
+# rankScoreRest = sorted(zip(rNames, map(lambda x: round(x, 4), list(rankScore))), reverse=True, key = lambda t: t[1])
+# print('The final sorted restaurant by ranks scores are shown below, higher values mean more compatible for the group.')
+# for r in rankScoreRest:
+#     print(r)
+# print('')
+#
+# print("----------------------------------------------------------------------------------------------------")
+# print("Why is there a difference between the two?  What problem arrives?  What does represent in the real world?")
+# print("")
+# print("The most compatible restaurant for the group is different between the raw sum & the sum on the order statistics")
+# print("")
+# print("This is driven by the fact that certain people have really high raw scores for the restaurant 'Recess' however, that rank value helps reduce the impact of those elevated scores")
+# print("")
+# print("In the real world this could be related to the people that may have really strong preferences can often times sway the entire group to their preferences.")
 
 
 # How should you preprocess your data to remove this problem.
 
 
+
 # Find user profiles that are problematic, explain why?
+
+fig, ax = plt.subplots(figsize=(10, 10))
+ax.set_title('Heatmap of Compatibility Scores Person by Restaurant (Raw Score)')  # name hist by variable
+plt.imshow(M_usr_x_rest)
+# We want to show all ticks
+ax.set_xticks(np.arange(len(rNames)))
+ax.set_yticks(np.arange(len(pNames)))
+#label them with the respective list entries
+ax.set_xticklabels(rNames)
+ax.set_yticklabels(pNames)
+
+# Rotate the tick labels and set their alignment.
+plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+         rotation_mode="anchor")
+
+# Loop over data dimensions and create text annotations.
+for i in range(len(pNames)):
+    for j in range(len(rNames)):
+        text = ax.text(j, i, M_usr_x_rest[i, j],
+                       ha="center", va="center", color="w")
+
+plt.show()
+fileName = str(projectName + '/HeatmapofPersonxRestaurantCompatibilityRawScores.png')  # assumes projectName exists from above
+plt.savefig(fileName)
+plt.close()
+
+
+fig, ax = plt.subplots(figsize=(10, 10))
+ax.set_title('Heatmap of Compatibility Scores Person by Restaurant (Rank Score)')  # name hist by variable
+plt.imshow(M_usr_x_rest_rank)
+# We want to show all ticks
+ax.set_xticks(np.arange(len(rNames)))
+ax.set_yticks(np.arange(len(pNames)))
+#label them with the respective list entries
+ax.set_xticklabels(rNames)
+ax.set_yticklabels(pNames)
+
+# Rotate the tick labels and set their alignment.
+plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+         rotation_mode="anchor")
+
+# Loop over data dimensions and create text annotations.
+for i in range(len(pNames)):
+    for j in range(len(rNames)):
+        text = ax.text(j, i, M_usr_x_rest_rank[i, j],
+                       ha="center", va="center", color="w")
+
+plt.show()
+fileName = str(projectName + '/HeatmapofPersonxRestaurantCompatibilityRankScores.png')  # assumes projectName exists from above
+plt.savefig(fileName)
+plt.close()
 
 
 # Think of two metrics to compute the disatistifaction with the group.
 
+print("First is the function where each person's actual score is subtracted from the groups averaged scores, squared, summed & averaged")
 
-# Should you split in two groups today?
+
+print("Second is the function where each person's actual ranking is subtracted from the groups average rating, squared, summed & averaged")
 
 
-# Ok. Now you just found out the boss is paying for the meal. How should you adjust. Now what is best restaurant?
+
+print("Should you split in two groups today?")
+
+
+
+print("Ok. Now you just found out the boss is paying for the meal. How should you adjust. Now what is best restaurant?")
+
+print("The best way to do this is convert everyones cost to 1 meaning cost is no longer an issue and determine the new scores")
 
 
 # Tommorow you visit another team. You have the same restaurants and they told you their optimal ordering for restaurants.  Can you find their weight matrix?
 
-
+print("Assuming you have everyone's individual rankings you can determine a similar weighting.")
 
