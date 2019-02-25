c4b25b7919e74b0e6810d732f810a8b14dc7d9c8
Wall <bsnxw01@railinc.com>
2019-02-25 09:55:53 -0500

    rank user / rest matrix

--- HW3/DecisionMakingWithMatrices.py
+++ HW3/DecisionMakingWithMatrices.py
@@ -8,6 +8,7 @@
 import random
 import names
 import numpy as np
+from scipy.stats import rankdata
 
 random.seed(3)
 
@@ -79,6 +80,7 @@ def matrixDict(dictName, names):
 
 M_people = matrixDict(people, pNames)
 print(M_people)
+print(M_people.dtype)
 print('')
 
 print("----------------------------------------------------------------------------------------------------")
@@ -119,6 +121,7 @@ print('')
 M_restaurants = matrixDict(restaurants, rNames)
 
 print(M_restaurants)
+print(M_restaurants.dtype)
 print('')
 
 # print("----------------------------------------------------------------------------------------------------")
@@ -141,34 +144,57 @@ print("Choose a person and compute(using a linear combination) the top restauran
 R = M_restaurants.view(int).reshape(len(M_restaurants),-1)
 P = M_people.view(int).reshape(len(M_people),-1)
 
-person0score = np.dot(R, P[0])
-
-restScore = sorted(zip(rNames, map(lambda x: round(x, 4), list(person0score))), reverse=True, key = lambda t: t[1])
-print('')
-print("The top restaurant for %s and score is:" % list(people.keys())[0])
-print(restScore[0])
-print('')
-print('Each score in the resulting vector represents their compatibility with that restaurant based on the similarity between their preferences and the restaurants characteristics")
-print('')
+# person0score = np.dot(R, P[0])
+#
+# restScore = sorted(zip(rNames, map(lambda x: round(x, 4), list(person0score))), reverse=True, key = lambda t: t[1])
+# print('')
+# print("The top restaurant for %s and score is:" % list(people.keys())[0])
+# print(restScore[0])
+# print('')
+# print("Each score in the resulting vector represents their compatibility with that restaurant based on the similarity between their preferences and the restaurants characteristics")
+# print('')
 print("----------------------------------------------------------------------------------------------------")
 print("Next compute a new matrix (M_usr_x_rest  i.e. an user by restaurant) from all people.  What does the a_ij matrix represent?")
 
+M_usr_x_rest = np.matmul(R, P.T)
+
+print(M_usr_x_rest)
+print('')
+print('Each ijth value represents an individual users score or preference for that type of restaurant with higher scores meaning a greater preference')
+print('')
 
 # Sum all columns in M_usr_x_rest to get optimal restaurant for all users.  What do the entry’s represent?
+rawScoreRest = {}
+for n in enumerate(rNames):
+    print(n[1])
+    print(sum(M_usr_x_rest[:,n[0]]))
+    rawScoreRest[n[1]] = sum(M_usr_x_rest[:,n[0]])
+
 
 # Now convert each row in the M_usr_x_rest into a ranking for each user and call it M_usr_x_rest_rank.   Do the same as above to generate the optimal resturant choice.
+# M_usr_x_rest_rank = np.zeros(len(rNames))
+for n in enumerate(rNames):
+    print(M_usr_x_rest[:, n[0]])
+    print(rankdata(M_usr_x_rest[:, n[0]], method='dense'))
+
 
 # Why is there a difference between the two?  What problem arrives?  What does represent in the real world?
 
+
 # How should you preprocess your data to remove this problem.
+
 
 # Find user profiles that are problematic, explain why?
 
+
 # Think of two metrics to compute the disatistifaction with the group.
+
 
 # Should you split in two groups today?
 
+
 # Ok. Now you just found out the boss is paying for the meal. How should you adjust. Now what is best restaurant?
+
 
 # Tommorow you visit another team. You have the same restaurants and they told you their optimal ordering for restaurants.  Can you find their weight matrix?
 
